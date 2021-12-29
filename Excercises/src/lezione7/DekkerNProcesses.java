14024cd71960f987f5266e924a17ae639a667ffe
Melon <matteobecatti7@gmail.com>
2021-12-29 11:32:46 +0100

    Done in a shitty way dekker algorithm with n = 3

---
+++ Excercises/src/lezione7/DekkerNProcesses.java
@@ -0,0 +1,55 @@
+package lezione7;
+
+public class DekkerNProcesses extends Thread
+{
+	private static volatile boolean[] want = {false, false, false};
+	private static volatile boolean[] passed = {false, false, false};
+	
+	int turn = 1;
+	
+	private String name;
+	private int id;
+	private boolean done = false;
+	public DekkerNProcesses(String name, int id) 
+	{
+		this.name = name;
+		this.id = id;
+	}
+	
+	public void run() 
+	{
+		for(int i = 0; i < 10; i++) //"loop forever"
+		{
+			System.out.println("NCS " + name); //NCS
+			want[id] = true;
+			while(!done) 
+			{
+				while(turn != id) 
+				{
+					if(!want[turn]) 
+					{
+						turn = id;
+					}
+				}
+				
+				passed[id] = true;
+				done = true;
+				for(int j = 1; j < 3; j++) 
+				{
+					if(passed[j]) 
+					{
+						passed[id] = false;
+						done = false;
+					} 
+				}
+			}
+			
+			System.out.println("Begin CS: " +  name); /* CS */
+	        System.out.println("End CS:   " +  name); /* CS */
+	        
+	        passed[id] = false;
+	        want[id] = false;
+		}
+	}
+	
+}
